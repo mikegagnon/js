e92120becb417c32090e4703307063a40a9eff59
Michael Gagnon <mikegagnon@gmail.com>
2019-09-28 13:25:33 -0700

    index.php and style

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>JavaScript: Overview &amp; Details</title>
+        <link rel="stylesheet" href="style/codemirror.css">
+        <link rel="stylesheet" href="style/solarized.css">
+        <link href="style/bootstrap.css" rel="stylesheet">
+        <link href="style/prism.css" rel="stylesheet">
+        <script src="js/codemirror.js"></script>
+        <script src="js/javascript.js"></script>
+        <script src="js/jquery-2.2.4.min.js"></script>
+        <script src="js/prism.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+    </head>
+    <body>
+        <main>
+        </main>
+    </body>
+</html>

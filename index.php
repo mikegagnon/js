1a4b3212fcb9fbdd9bdd6b927d2f286b92a41374
Michael Gagnon <mikegagnon@gmail.com>
2019-09-28 13:45:49 -0700

    title

--- index.php
+++ index.php
@@ -1,22 +1,35 @@
 <!doctype html>
-<html lang="en">
+<html lang='en'>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
         <title>JavaScript: Overview &amp; Details</title>
-        <link rel="stylesheet" href="style/codemirror.css">
-        <link rel="stylesheet" href="style/solarized.css">
-        <link href="style/bootstrap.css" rel="stylesheet">
-        <link href="style/prism.css" rel="stylesheet">
-        <script src="js/codemirror.js"></script>
-        <script src="js/javascript.js"></script>
-        <script src="js/jquery-2.2.4.min.js"></script>
-        <script src="js/prism.js"></script>
-        <script src="js/popper.min.js"></script>
-        <script src="js/bootstrap.js"></script>
+        <link rel='stylesheet' href='style/codemirror.css'>
+        <link rel='stylesheet' href='style/solarized.css'>
+        <link href='style/bootstrap.css' rel='stylesheet'>
+        <link href='style/prism.css' rel='stylesheet'>
+        <link href='style/book.css' rel='stylesheet'>
+        <link href='style/sidenote.css' rel='stylesheet'>
+        <script src='js/codemirror.js'></script>
+        <script src='js/javascript.js'></script>
+        <script src='js/jquery-2.2.4.min.js'></script>
+        <script src='js/prism.js'></script>
+        <script src='js/popper.min.js'></script>
+        <script src='js/bootstrap.js'></script>
     </head>
     <body>
         <main>
+<div data-column='0' class='column'>
+    <div class='title-page'>
+        <h1 class='title'>JavaScript</h1>
+        <div class='subtitle'>Overview &amp; Details</div>
+        <div class='subtitle'>Volume 1</div>
+        <div class='author'>Michael N. Gagnon, 2019</div>
+    </div>
+</div>
+
+<div id='staging-area'>
+</div>
         </main>
     </body>
 </html>

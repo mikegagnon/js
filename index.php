ddd06d19dc412c7f3d95c2be07ce12c4906c183d
Michael Gagnon <mikegagnon@gmail.com>
2019-10-03 10:19:07 -0700

    indetation

--- index.php
+++ index.php
@@ -1636,15 +1636,15 @@ stepheader('note-identifiers', 'Valid variable names and identifiers'); ?>
 
 <? #############################################################################
 stepheader('note-assignment-op', 'Assignment operator'); ?>
-        <p>In <? steplink('note-var') ?>, we saw how to declare new variables. In this step, though, we show how to make an existing variable refer to a new value. </p>
+    <p>In <? steplink('note-var') ?>, we saw how to declare new variables. In this step, though, we show how to make an existing variable refer to a new value. </p>
 
-        <p>It&rsquo;s very simple. Let&rsquo;s say you&rsquo;ve already declared <code class="language-javascript">var x = 1</code>, and now you would like to have <code class="language-javascript">x</code> refer to the value <code class="language-javascript">'Hello'</code>. Simply execute <code class="language-javascript">x = 'Hello'</code>.</p>
+    <p>It&rsquo;s very simple. Let&rsquo;s say you&rsquo;ve already declared <code class="language-javascript">var x = 1</code>, and now you would like to have <code class="language-javascript">x</code> refer to the value <code class="language-javascript">'Hello'</code>. Simply execute <code class="language-javascript">x = 'Hello'</code>.</p>
 
-        <p>As you might expect, the right-hand side of the <code class="language-javascript">=</code> sign can be any valid expression. For instance: <code class="language-javascript">x = true || false</code>.</p>
+    <p>As you might expect, the right-hand side of the <code class="language-javascript">=</code> sign can be any valid expression. For instance: <code class="language-javascript">x = true || false</code>.</p>
 
-        <p>You can even include the variable name on the right-hand side. For instance, if you first declare <code class="language-javascript">var x = 1</code>, then you could follow up with <code class="language-javascript">x = x + 4</code>, and then <code class="language-javascript">x</code> would refer to the value <code class="language-javascript">5</code>. The way it works, is JavaScript first resolves the expression on the right-hand side, and then sets the variable to refer to the resulting value.</p>
+    <p>You can even include the variable name on the right-hand side. For instance, if you first declare <code class="language-javascript">var x = 1</code>, then you could follow up with <code class="language-javascript">x = x + 4</code>, and then <code class="language-javascript">x</code> would refer to the value <code class="language-javascript">5</code>. The way it works, is JavaScript first resolves the expression on the right-hand side, and then sets the variable to refer to the resulting value.</p>
 <? stepoverview(); ?>
-
+    <code class="language-javascript">x = x + 4</code> adds 4 to the value of <code class="language-javascript">x</code>
 <? stepfooter(); ?>
 
 <? ########################################################################## ?>

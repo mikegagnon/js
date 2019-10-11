e7a21ec016aa5236d9e26b69bed537b15f6cf948
Michael Gagnon <mikegagnon@gmail.com>
2019-10-11 15:39:23 -0700

    two-player ttt

--- vol2/ttt/complete/ttt.js
+++ vol2/ttt/complete/ttt.js
@@ -7,11 +7,67 @@ function cellClick(locationNumber) {
         boardState[locationNumber] = currentPlayer
         var text = document.createTextNode(currentPlayer)
         document.querySelector('#location-' + locationNumber).appendChild(text)
+
+        if (checkVictory(currentPlayer)) {
+            alert(currentPlayer + ' wins!')
+        } else if (checkTie()) {
+            alert('Tie!')
+        }
+
         if (currentPlayer === 'X') {
             currentPlayer = 'O'
         } else {
             currentPlayer = 'X'
         }
+    }
+}
+
+function checkTie() {
+    for (var i = 0; i < boardState.length; i++) {
+        if (boardState[i] === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function checkVictory(player) {
+    if (boardState[0] === player &&
+        boardState[1] === player &&
+        boardState[2] === player) {
+        return true
+    } else if (boardState[3] === player &&
+        boardState[4] === player &&
+        boardState[5] === player) {
+        return true
+    } else if (boardState[6] === player &&
+        boardState[7] === player &&
+        boardState[8] === player) {
+        return true
+    } else if (boardState[0] === player &&
+        boardState[3] === player &&
+        boardState[6] === player) {
+        return true
+    } else if (boardState[1] === player &&
+        boardState[4] === player &&
+        boardState[7] === player) {
+        return true
+    } else if (boardState[2] === player &&
+        boardState[5] === player &&
+        boardState[8] === player) {
+        return true
+    } else if (boardState[0] === player &&
+        boardState[4] === player &&
+        boardState[8] === player) {
+        return true
+    } else if (boardState[2] === player &&
+        boardState[4] === player &&
+        boardState[6] === player) {
+        return true
+    }
+    else {
+        return false
     }
 }
 
